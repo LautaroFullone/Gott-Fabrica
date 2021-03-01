e510f484d7891dce4c69c4c8d68086ebad7732b0
LautaroFullone <lautarofullone@gmail.com>
2021-03-01 11:14:10 -0300

    Header 1

---
+++ resources/views/layoutsWeb/contact.blade.php
@@ -0,0 +1,6 @@
+<div class="contact-section">
+    <div class="text">Queremos conocer tus metas<br>y seguir en contacto</div>
+    <div class="button">
+      <a href="">CONTACTAR</a>
+    </div>
+  </div>
---
+++ resources/views/layoutsWeb/instagram-feed.blade.php
@@ -0,0 +1,9 @@
+<div class="instagram">
+    <div class="section-title">Instagram</div>
+    <div class="instagram-content">
+
+        <div id="instafeed-container"></div>
+
+    </div>
+</div>
+
